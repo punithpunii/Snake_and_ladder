e036535058438525a605d49152cb25552b362bef
punithpunii <punithmurthy2021@gmail.com>
2024-12-18 11:16:57 +0530

    Added UC5 get_exact_win_pos

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-#use case 4:  Repeat till the Player reaches the winning position 100
+#use case 5:  Ensure the player gets to exact winning position 100
 
 import random
 
 def play_game():
     player1_pos=0
     p2_dice_rolled_count=0
+    while True:
+        print("Player 1 turn, pos :",player1_pos)
+        player1_pos=snakeladder(player1_pos)
+        p1_dice_rolled_count+=1
+        if player1_pos==100:  #use case 5 
+            print("Player 1 won by reaching 100 first")
+            break
 play_game()
 
 def snakeladder(pos):
@@ -15,7 +22,7 @@ def snakeladder(pos):
     if pos<100:
         print("Current position :",pos)
         pos=snakeorladder_check(pos)
-    if pos>100:
+    if pos>100: #the player stays in the same position till the player gets the exact number that adds to 100
         pos-=dice
         print("Current position :",pos)
         pos=snakeorladder_check(pos)
@@ -26,13 +33,13 @@ def snakeorladder_check(pos):
  
     check_roll=random.randint(1,3)
 
-    if check_roll==1:
+    if check_roll==1: # Snake
         return event_snake(pos)
 
-    elif check_roll==2 and pos<=94:
+    elif check_roll==2 and pos<=94:   # Ladder
         return event_ladder(pos)
-
-    return pos
+    else:  # No Play
+        return pos
 
 def event_snake(pos):
     print("  Encountered snake at pos:",pos)
