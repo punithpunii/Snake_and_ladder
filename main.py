bc96311ff062a684a231f9a40c92acade1106a94
punithpunii <punithmurthy2021@gmail.com>
2024-12-18 10:50:39 +0530

    Added_UC3_snake_ladder_noplay

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-#use case 1: The Player rolls the die to get a number between 1 to 6 
+#use case 3:  Player then checks for No Play, Ladder or Snake
 
-def snakeladder(pos):
+import random
 
-    dice=random.randint(1,6)
-    print("Rolled:",dice)
-    pos+=dice
-    if pos<100:
-        print("Current position :",pos)
-        pos=snakeorladder_check(pos)
-    if pos>100:
-        pos-=dice
-        print("Current position :",pos)
-        pos=snakeorladder_check(pos)
-        
+def snakeorladder_check(pos):
+ 
+    check_roll=random.randint(1,3)
+
+    if check_roll==1:
+        return event_snake(pos)
+
+    elif check_roll==2 and pos<=94:
+        return event_ladder(pos)
+
     return pos
+
+def event_snake(pos):
+    print("  Encountered snake at pos:",pos)
+    snake_roll=random.randint(1,6)
+    print("  snake_rolled",snake_roll)
+    pos-=snake_roll
+    print("  After snake encounterd, position is:",pos)
+    return pos
+
+def event_ladder(pos):
+    print("  Encountered ladder at pos:",pos)
+    ladder_up=random.randint(1,6)
+    print("  ladder_rolled",ladder_up)
+    pos+=ladder_up
+    print("  After ladder encountered, position is:",pos)
+    return pos
+
