102bd1d0515f5e5148ed453aa39aaf2e458acb28
punithpunii <punithmurthy2021@gmail.com>
2024-12-18 10:34:31 +0530

    Added_UC2_with_roll_die

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-def play_game():
-    player1_pos=0
-    p2_dice_rolled_count=0
+#use case 1: The Player rolls the die to get a number between 1 to 6 
+
+def snakeladder(pos):
+
+    dice=random.randint(1,6)
+    print("Rolled:",dice)
+    pos+=dice
+    if pos<100:
+        print("Current position :",pos)
+        pos=snakeorladder_check(pos)
+    if pos>100:
+        pos-=dice
+        print("Current position :",pos)
+        pos=snakeorladder_check(pos)
+        
+    return pos
